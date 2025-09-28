23aab3ac801f87f2126dfd93f16a6fccaf42d7ba
Mounir Loughribi <loughribimounir@gmail.com>
2025-09-28 22:26:44 +0200

    bedtand tijdelijk.py gecodeerd

---
+++ tijdelijk.py
@@ -0,0 +1,14 @@
+prijzen = {
+    "aardbij": 3,
+    "vanille": 4,
+    "chocolade": 5
+}
+aanbiding = prijzen["aardbij"] * 0.8
+reclame_tekst = f"Vandaag in de aanbiding: vanille-ijs, 1 liter - slechts € {aanbiding}"
+reclame_tekst2 = reclame_tekst[:36]
+reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst4 = reclame_tekst3.split()
+for el in reclame_tekst4:
+    print(el.lower())
+    if len(el) >= 5:
+        print(el.upper())
